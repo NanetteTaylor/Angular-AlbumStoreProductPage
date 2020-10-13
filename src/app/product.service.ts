da50bf45edf613ce01afd62bb7f7520829959957
nanettetaylor <nanette.m.taylor@gmail.com>
2020-10-13 20:27:39 +0000

    Finished Part 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 // import { map } from 'rxjs/operators';
 
 
@@ -12,8 +14,8 @@ export class ProductService {
   constructor(private _http: Http) {
   }
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response: Response) => response.json());
+  getAlbum(id: number): Observable<Album>{
+     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 
 }
